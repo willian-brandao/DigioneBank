930d17e499da67bf6da3a45122efe92b32cb8400
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-01 00:11:59 -0300

    update to the property of attribute id a public to private

--- src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -3,17 +3,15 @@ package one.digitalinnovation.digionebank
 class Person {
     var name: String = "Willian"
     var id: String = "123.123.123-11"
+    private set
 
-    inner class Address{
-        var street = "Test Street"
-
-    }
 }
 
 fun main() {
     val p = Person()
 
+
     println(p.name)
     println(p.id)
-    println(p.Address().street)
+
 }
