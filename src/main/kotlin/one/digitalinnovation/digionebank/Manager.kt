917d722cee76eab1a0e859b4450276f573b284b1
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-01 04:22:38 -0300

    update class manager

--- src/main/kotlin/one/digitalinnovation/digionebank/Manager.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Manager.kt
@@ -3,7 +3,10 @@ package one.digitalinnovation.digionebank
 class Manager(
     name: String,
     id: String,
-    salary: Double
-) : Employee(name= name, id = id, salary = salary) {
+    salary: Double,
+    val password: String
+) : Employee(name= name, id = id, salary = salary), Logged {
     override fun bonusCalculation(): Double = salary * 0.4
+
+    override fun login(): Boolean = "password123" == password
 }
