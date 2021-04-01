9ccfd269a63ccb9b3967cc341873b6de1aa28e92
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-03-31 23:57:26 -0300

    create class person

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Person {
+    var name: String = "Willian"
+    var id: String = "123.123.123-11"
+}
+
+fun main() {
+    val p = Person()
+
+    println(p.name)
+    println(p.id)
+}
