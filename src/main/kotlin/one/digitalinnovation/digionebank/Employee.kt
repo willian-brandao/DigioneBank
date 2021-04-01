b9f8976656322176f58e4d73d45baf4a24e15965
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-01 02:03:25 -0300

    update make  person  and employee abstract classes

--- src/main/kotlin/one/digitalinnovation/digionebank/Employee.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Employee.kt
@@ -2,10 +2,10 @@ package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
 
-class Employee(
-    override val name: String,
-    override val id: String,
+abstract class Employee(
+    name: String,
+    id: String,
     val salary : BigDecimal
 ) : Person(name, id) {
-
+    abstract fun bonusCalculation();
 }
--- src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -1,7 +1,7 @@
 package one.digitalinnovation.digionebank
 
 //use term "open" to able those others classes to use the properties of person by inheritance
-open class Person (
+abstract class Person (
     var name: String,
     var id: String
 ){
