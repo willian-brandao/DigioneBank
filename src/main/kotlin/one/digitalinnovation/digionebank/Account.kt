336871228b3722892e3f262253c6c8a6296153ef
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-01 00:30:12 -0300

    created account class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Account.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Account(
+    val agency: String,
+    val id: String,
+    val balance: BigDecimal
+){
+    //behaviors
+    fun deposit(value: BigDecimal){
+
+    }
+    fun withdraw(value: BigDecimal){
+
+    }
+}
